d70f4336e2b4d805d6f90bbc1ca02005717e40ac
Sean Taylor <silverwolf4350@gmail.com>
2020-09-09 21:17:41 +0000

    Created stats endpoint.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,15 +5,31 @@ creates route /status for blueprint object app_views
 
 from flask.json import jsonify
 from api.v1.views import app_views
-import json
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def show_status():
     """
     returns JSON: "status": "OK"
     """
-    # TO DO:
-    # Needs to be JSON string - currently working with just regular string
-    # Attempting to use json.dumps to save as JSON, but missing positional arg
-    # json.dumps('"status": "OK"')
-    return (jsonify({'status': 'OK'}))
+    return jsonify(status='OK')
+
+@app_views.route('/stats', strict_slashes=False)
+def show_stats():
+    """
+    Retrieves number of objects by type
+    """
+    stats = {'amenities': storage.count(Amenity),
+             'cities': storage.count(City),
+             'places': storage.count(Place),
+             'reviews': storage.count(Review),
+             'states': storage.count(State),
+             'users': storage.count(User)}
+
+    return jsonify(stats)
