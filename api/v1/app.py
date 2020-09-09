20a99f1664ad3f07adeab3603720195d077cb3c7
Sean Taylor <silverwolf4350@gmail.com>
2020-09-09 21:22:45 +0000

    Created 404 error handler.

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ creates instance of Flask and registers blueprint to instance
 """
 
 from flask import Flask
+from flask.json import jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -18,6 +19,13 @@ def teardown(self):
     closes current session through storage.close()
     """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(e):
+    """
+    returns json formatted error
+    """
+    return jsonify(error='Not found'), 404
 
 if __name__ == "__main__":
     app.run(host=host_var ,port=port_var, threaded=True)
