abdab61bcfc8b31ce883cc7ca5fbbd327f4836d5
Sean Taylor <silverwolf4350@gmail.com>
2020-09-10 19:25:30 +0000

    Created amenities API endpoint.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,7 @@ creates app_views, an instance of Blueprint
 """
 
 from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.amenities import *
